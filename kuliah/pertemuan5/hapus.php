4e5a6bb5f77a1b49b64c02fb3ff0762c1e8f4296
dwi312 <dwi31287no5@gmail.com>
2021-11-11 20:37:48 +0700

    Menambahkan fitur LOGIN

---
+++ kuliah/pertemuan5/hapus.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['login'])) {
+    header("Location: login.php");
+    exit;
+}
+
+require 'functions.php';
+
+if (!isset($_GET['id'])) {
+    header("Location: index.php");
+    exit;
+}
+
+$id = $_GET['id'];
+
+if (hapus($id) > 0) {
+    echo "<script>
+                alert('Data berhasil dihapus');
+                document.location.href = 'index.php';
+            </script>";
+} else {
+    echo "<script>
+                alert('Data gagal dihapus!');
+                document.location.href = 'index.php';
+            </script>";
+}
